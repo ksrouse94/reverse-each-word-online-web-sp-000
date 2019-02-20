8f5d6425d228e92f54d8d33a63be162183d793ff
Kimberly Rouse <ksrouse94@gmail.com>
2019-02-20 00:38:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,4 @@
 def reverse_each_word(str)
   sentence = str.split("")
-  reverse_sentence = []
-  sentence.each do |word|
-    word.reverse!
-    reverse_sentence << word
-  end
-  reverse_sentence
+  puts sentence
 end  
